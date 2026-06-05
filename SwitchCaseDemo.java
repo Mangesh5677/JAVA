248defa4a9da11804ff646e2f0f04abedaa9ec54
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-05 19:44:30 +0530

    Initial commit

--- SwitchCaseDemo.java
+++ SwitchCaseDemo.java
@@ -1,6 +1,6 @@
 public class SwitchCaseDemo {
     public static void main(String[] args){
-        int day = 8;
+        int day = 2;
         switch(day){
             case 1:
                 System.out.println("Monday");
