c589cc964c2e57a659784b6f783729949017e07b
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-05 19:28:54 +0530

    Initial commit

---
+++ TernaryDemo.java
@@ -0,0 +1,8 @@
+public class TernaryDemo{
+    public static void main(String[] args){
+        int x = 10;
+        int y =20;
+        int min = (x<y) ? x : y;
+        System.out.println("Minimum value is: " + min);
+    }
+}
