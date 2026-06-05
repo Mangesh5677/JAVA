3ad1f172b75a9127290c418007ad5536619d5ee4
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-05 20:14:11 +0530

    Initial commit

---
+++ NestedWhile.java
@@ -0,0 +1,15 @@
+public class NestedWhile {
+    public static void main(String[] args)
+    {
+        int i = 1;
+        while(i<=5){
+            System.out.println("Hii"+ i);
+            int j=1;
+            while(j<=3){
+                System.out.println("Hello"+j);
+                j++;
+            }
+            i++;
+        }
+    }
+}
