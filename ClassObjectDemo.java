0e743566fb2dcc748c7f57d681acdb24bae520a7
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-08 20:01:30 +0530

    Initial commit

--- ClassObjectDemo.java
+++ ClassObjectDemo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Calculator {
     public int add(int a, int b) {
         return a + b;
@@ -6,8 +8,13 @@ class Calculator {
 
 public class ClassObjectDemo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter Second number: ");
+        int num2 = sc.nextInt();
         Calculator calc = new Calculator();
-        int result = calc.add(20, 10);
+        int result = calc.add(num1, num2);
         System.out.println("Sum: " + result);
     }
 }
