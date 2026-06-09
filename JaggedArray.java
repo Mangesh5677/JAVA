b03c1bdf19b4929b154972c4a00d4de8baa536a7
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-09 20:07:43 +0530

    Initial commit

--- JaggedArray.java
+++ JaggedArray.java
@@ -5,9 +5,9 @@ public class JaggedArray {
         nums[1] = new int[3];
         nums[2] = new int[4];
 
-         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums[i].length; j++) {
-                nums[i][j] = (int)(Math.random() * 100);
+        for (int[] num : nums) {
+            for (int j = 0; j < num.length; j++) {
+                num[j] = (int)(Math.random() * 100);
             }
         }
         for(int n[]: nums){
