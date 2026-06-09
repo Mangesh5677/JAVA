f13f08630bd122b7588f2778a12da13736b265d7
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-09 19:19:02 +0530

    Initial commit

---
+++ ArrayDemo2.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class ArrayDemo2 {
+    public static void main(String[] args) {
+       int num[] = new int[5];
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter 5 numbers");
+       for(int i=0;i<num.length;i++){
+        num[i] = sc.nextInt();
+       }
+       System.out.println("The numbers are:");
+       for(int i=0;i<num.length;i++){
+        System.out.print(num[i] + " ");
+       }
+    }
+}
