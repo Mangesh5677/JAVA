405eec33691d39ae81cc99a21615764632101f05
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-11 20:32:12 +0530

    Initial commit

--- StringDemo.java
+++ StringDemo.java
@@ -2,5 +2,7 @@ public class StringDemo {
     public static void main(String[] args) {
        String name = new String("Mangesh");
        System.out.println(name);
+    //    System.out.println(name.hashCode());
+    
     }
 }
