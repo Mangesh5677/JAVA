a6aee7dd3a7c40b108db3d3c7f2cf9213d0f91cb
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-11 20:35:24 +0530

    Initial commit

--- StrBuffer.java
+++ StrBuffer.java
@@ -1,6 +1,8 @@
 public class StrBuffer {
     public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer("Hello");
-        System.out.println(sb.length());
+        StringBuffer sb = new StringBuffer("Mangesh ");
+        // System.out.println(sb.length());
+        sb.append("Shinde");
+        System.out.println(sb);
     }
 }
