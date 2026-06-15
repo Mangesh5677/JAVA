11a8caa8a92d5904bcb227b9c08fa8c010c16dbd
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-15 20:18:00 +0530

    Initial commit

--- Core Java/StaticVAriable.java
+++ Core Java/StaticVAriable.java
@@ -21,6 +21,6 @@ public class StaticVAriable {
 
         Mobile.name="Phone";
         obj1.show();
-        obj2.show();
+        obj2.show();//Static variable is shared by all the objects of the class. It belongs to the class and not to any specific object. When we change the value of a static variable, it changes for all the objects that reference it. In this example, when we set Mobile.name to "Phone", it changes the name for both obj1 and obj2, which is why both show() methods will print "Phone" as the name. 
     }
 }
