e7f453722cac21b4efd465d872da035a9a19d1e7
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-15 19:28:07 +0530

    Initial commit

--- StaticVAriable.java
+++ StaticVAriable.java
@@ -1,7 +1,7 @@
 class Mobile{
   String brand;
   int price;
-  String name;
+  static String name;
   public void show(){
     System.out.println(brand+":"+price+":"+name);
   }
@@ -12,13 +12,15 @@ public class StaticVAriable {
         Mobile obj1 = new Mobile();
         obj1.brand="Apple";
         obj1.price=100000;      
-        obj1.name="Iphone 14 Pro Max";
-        obj1.show();
-
+        Mobile.name="Iphone 14 Pro Max";
+        
         Mobile obj2 = new Mobile();
         obj2.brand="Samsung";
         obj2.price=80000;   
-        obj2.name="Samsung Galaxy S23 Ultra";
+        Mobile.name="Samsung Galaxy S23 Ultra";
+
+        Mobile.name="Phone";
+        obj1.show();
         obj2.show();
     }
 }
