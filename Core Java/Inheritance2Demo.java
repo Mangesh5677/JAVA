e133b41bf960e870e39136d3a9b73fb90df34b52
Mangesh shinde <mangeshshinde61499@gmail.com>
2026-06-16 20:17:26 +0530

    upadate

---
+++ Core Java/Inheritance2Demo.java
@@ -0,0 +1,22 @@
+class A {
+    int add(int n1, int n2) {
+        return n1 + n2;
+    }
+}
+
+class B extends A {
+    int sub(int n1, int n2) {
+        return n1 - n2;
+    }
+}   // <-- Close class B here
+
+public class Inheritance2Demo {
+    public static void main(String[] args) {
+        B obj = new B();
+        int sum = obj.add(10, 5);
+        int difference = obj.sub(10, 5);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+    }
+}
